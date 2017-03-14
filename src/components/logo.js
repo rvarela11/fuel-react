4cf729b7454cdec4521d7516830d5ecda35f9691
Robert Varela <robert.varela@rvarela-ret.local>
2017-03-14 12:33:53 -0500

    redesigned header and home page

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,23 +4,19 @@ import { connect } from 'react-redux';
 class Logo extends Component {
   render(){
     return <div>
-      <div className="logoDivOutside">
-        <div className="logoDivInside">
-          <div className="imageDivOutside">
-            <div className="image"><h1>FUEL</h1></div>
-          </div>
-          <div className="outsideCaloriesDiv">
-            <div className="insideCaloriesDiv">
+        <header>
+          <h1>FUEL</h1>
+          <div className="calories">
+            <div>
               <h4>Goal</h4>
-              <h4><strong><span className="totalCaloriesDisplayClass" id="totalCaloriesDisplayGoal">{this.props.goal}</span></strong></h4>
+              <h4><strong><span id="totalCaloriesDisplayGoal">{this.props.goal}</span></strong></h4>
             </div>
-            <div className="insideCaloriesDiv">
+            <div>
               <h4>Daily</h4>
-              <h4><strong><span className="totalCaloriesDisplayClass" id="totalCaloriesDisplayDaily">{this.props.daily}</span></strong></h4>
+              <h4><strong><span id="totalCaloriesDisplayDaily">{this.props.daily}</span></strong></h4>
             </div>
           </div>
-        </div>
-      </div>
+        </header>
     </div>
   }
 }
